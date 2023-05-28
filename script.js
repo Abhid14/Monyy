d29b059c042349a820335aa85b188337ca5e6f49
Abhishek Das <abhid14@outlook.com>
2023-05-28 22:59:37 +0530

    some work

--- script.js
+++ script.js
@@ -1,35 +1,193 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const childElement1 = document.querySelector('.hero-showcase');
-const childElement2 = document.querySelector('.hero-1-marquee-horizontal');
+gsap.to(".empty-mock, .hero-mock-1", {
+  y:0,
+  rotation: 0,
+  duration: 1,
+});
 
-gsap.to(".hero-showcase", {
-  y: -500,
-  scrollTrigger: {
-    trigger: '.hero-1',
-    scrub: true,
-    start: 'top top',
-    end: 'bottom top',
-  },
+gsap.to(".empty-mock, .hero-mock-1", {
+  x:0,
+  rotation: 0,
+  duration: 4,
 });
-gsap.to(".hero-2", {
-  y: -400,
-  scrollTrigger: {
-    trigger: '.hero-1',
-    scrub: true,
-    start: 'top top',
-    end: 'bottom top',
-  },
-});
-gsap.to(childElement2, {
-  y: 40,
+
+// 150.72 82.4 40 margin
+
+gsap.to(".hero-1-marquee-horizontal", {
   opacity: 0,
+  y: 40,
   scrollTrigger: {
-    trigger: '.hero-1',
+    trigger: ".hero-1",
     scrub: true,
-    start: 'top top',
-    end: 'bottom 60%',
+    start: "top top",
+    end: "bottom 60%",
+  },
+});
+gsap.to(".container-hero-1", {
+  y: -200,
+  scrollTrigger: {
+    trigger: ".hero-1",
+    scrub: true,
+    start: "top top",
+    end: "bottom top",
+  },
+});
+// gsap.to(".hero-mock-center", {
+//   y: -350,
+//   scrollTrigger: {
+//     trigger: ".hero-1",
+//     scrub: true,
+//     start: "top top",
+//     end: "bottom top",
+//   },
+// });
+// gsap.to(".hero-mock-1", {
+//   y: -290,
+//   scrollTrigger: {
+//     trigger: ".hero-1",
+//     scrub: true,
+//     start: "top top",
+//     end: "bottom top",
+//   },
+// });
+// gsap.to(".empty-mock", {
+//   y: -250,
+//   scrollTrigger: {
+//     trigger: ".hero-1",
+//     scrub: true,
+//     start: "top top",
+//     end: "bottom top",
+//   },
+// });
+
+gsap.fromTo(
+  ".sel-country-card-1",
+  {
+    y: 0,
+  },
+  {
+    y: 0,
+    scrollTrigger: {
+      trigger: ".hero-1",
+      scrub: true,
+      start: "top top",
+      end: "bottom top",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".sel-country-card-2",
+  {
+    y: 400,
+  },
+  {
+    y: 0,
+    scrollTrigger: {
+      trigger: ".hero-1",
+      scrub: true,
+      start: "top top",
+      end: "bottom top",
+    },
+  }
+);
+
+gsap.to(".hero-2", {
+  opacity: 0,
+  y: -230,
+  scrollTrigger: {
+    trigger: ".hero-3",
+    scrub: true,
+    start: "top bottom",
+    end: "bottom bottom",
   },
 });
 
+gsap.fromTo(
+  ".hero-3",
+  {
+    y: 100,
+  },
+  {
+    y: -100,
+    scrollTrigger: {
+      trigger: ".hero-2",
+      scrub: true,
+      start: "top bottom",
+      end: "bottom bottom",
+    },
+  }
+);
 
+gsap.fromTo(
+  ".hero-3 >:nth-child(1)",
+  {
+    y: 100,
+  },
+  {
+    y: -20,
+    scrollTrigger: {
+      trigger: ".hero-2",
+      scrub: true,
+      start: "top bottom",
+      end: "bottom bottom",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".hero-3 >:nth-child(2)",
+  {
+    y: 50,
+  },
+  {
+    y: 0,
+    scrollTrigger: {
+      trigger: ".hero-3",
+      scrub: true,
+      start: "top bottom",
+      end: "bottom bottom",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".hero-3 >:nth-child(3)",
+  {
+    y: 100,
+  },
+  {
+    y: 0,
+    scrollTrigger: {
+      trigger: ".hero-3",
+      scrub: true,
+      start: "top bottom",
+      end: "bottom bottom",
+    },
+  }
+);
+
+gsap.fromTo(
+  ".section-4-cards-container",
+  { y: 0 }, // Initial y position
+  {
+    y: "-500vh",
+    scrollTrigger: {
+      trigger: ".section-4",
+      scrub: true,
+      pin: true,
+      start: "top top",
+      end: "+=400%",
+    },
+  }
+);
+gsap.from(".section-4", {
+  height: "100vh", // Initial y position
+  scrollTrigger: {
+    trigger: ".section-5",
+    scrub: true,
+    start: "top top",
+    end: "+=400%",
+  },
+});
